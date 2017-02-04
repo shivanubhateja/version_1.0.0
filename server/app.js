7b93c79588bca9fd5bd941a97bc4962fd76117c3
shivanubhateja <shivanubhateja31@gmail.com>
2017-02-04 15:52:29 +0530

    msg91

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var express = require('express');
 var path = require('path');
-var http = require('http');
+var https = require('https');
 var promise = require('promise');
 var nodemailer = require('nodemailer');
 var fs = require('fs');
@@ -562,21 +562,42 @@ app.post('/submitRequest',function(request,response){
 	   				// 	// response.send({signUpResponse:"emailSent"});
 	   				// 	}
     			});
-    			var options = {
-					  host: "https://control.msg91.com",
-					  port: 80,
-					  path: '/api/sendhttp.php?authkey=139030A46PfUBW588ee201&mobiles=9962702609&message=bcchalgya&sender=99999&route=4&country=91',
-					  method: 'GET'
-					};
 
-					http.request(options, function(res) {
-					  console.log('STATUS: ' + res.statusCode);
-					  console.log('HEADERS: ' + JSON.stringify(res.headers));
-					  res.setEncoding('utf8');
-					  res.on('data', function (chunk) {
-					    console.log('BODY: ' + chunk);
-					  });
-					}).end();
+
+
+    			//msg91
+    	// 		var options = {
+					//   host: "https://control.msg91.com",
+					//   path: '/api/sendhttp.php?authkey=139030A46PfUBW588ee201&mobiles=9962702609&message=bcchalgya&sender=99999&route=4&country=91',
+					//   method: 'GET'
+					// };
+
+					// http.request(options, function(res) {
+					//   console.log('STATUS: ' + res.statusCode);
+					//   console.log('HEADERS: ' + JSON.stringify(res.headers));
+					//   res.setEncoding('utf8');
+					//   res.on('data', function (chunk) {
+					//     console.log('BODY: ' + chunk);
+					//   });
+					// }).end();
+
+
+   https.get('https://control.msg91.com/api/sendhttp.php?authkey=139030A46PfUBW588ee201&mobiles='+deviceDetails.tel.primary+'&message=Booking%20successful,%20your%20assigned%20engineer%20will%20contact%20you%20soon.%20Thank%20You&sender=Clorda&route=4&country=91', function(res){
+        var str = '';
+        console.log('Response is '+res.statusCode);
+
+        res.on('data', function (chunk) {
+              //console.log('BODY: ' + chunk);
+               str += chunk;
+         });
+
+        res.on('end', function () {
+             console.log(str);
+        });
+
+  });
+
+
         	response.send({response:"requestSubmitted", data:submitRequests})
         } 
         // return console.error("Error while saving data to MongoDB: " + err); // <- this gets executed when there's an error
